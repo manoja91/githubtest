8790c46061bf49aac9491104a1ec07ef22934c7f
manoja91 <manoja.venkata@gmail.com>
2023-03-07 13:56:47 -0600

    Update StringSumofNum.java

--- StringSumofNum.java
+++ StringSumofNum.java
@@ -3,13 +3,19 @@ public class StringSumofNum
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		int finalSum=0;
+		int finalSumVal=0;
 		String testVal ="Man1oja52391";
 		String numberVal = testVal.replaceAll("\\D","");
 		System.out.println(numberVal);
 		for(int i=0; i<numberVal.length();i++){
-		    //System.out.println(numberVal.charAt(i));
-		    finalSum= finalSum+numberVal.charAt(i);
+		    System.out.println(numberVal.charAt(i));
+		    finalSum= finalSum+Character.getNumericValue(numberVal.charAt(i));
 		}
-	 System.out.println(finalSum);	
+	 System.out.println(finalSum);
+	 for(int j=0; j<testVal.length(); j++){
+	     if(Character.isDigit(testVal.charAt(j))){
+	         finalSumVal = finalSumVal+testVal.charAt(j);
+	         }
+	        }System.out.println(finalSum);
 	}
 }
